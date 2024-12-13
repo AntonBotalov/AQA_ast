887da761031ec7f42a0f1382f531ac39ce69e4a9
Anton Botalov <antitoxa23@gmail.com>
2024-12-13 17:14:15 +0500

    add testng

--- Lesson_14/src/test/java/test.java
+++ Lesson_14/src/test/java/test.java
@@ -1,21 +1,24 @@
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
 public class test {
+    @DataProvider
+    public static Object[][] parserLocaleData() {
+        return new Object[][]{
+                {1,1},
+                {2,2},
+                {3,6}
+        };
+    }
+
     @Test
-    @DisplayName("Проверка n = 5")
     void test1() {
         assertEquals((Factorial.factorial(5)), 120);
     }
 
-    @ParameterizedTest
-    @CsvSource({"1, 1", "2, 2", "3, 6"})
+    @Test(dataProvider = "parserLocaleData")
     void test2(int first, int second) {
         assertEquals(Factorial.factorial(first),second);
     }
